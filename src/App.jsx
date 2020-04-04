90dc9ef030a4e4b75e5c3074b96bbf68973b4250
Marian <marianmrva123@gmail.com>
2020-04-04 16:28:53 +0200

    Add introduction screen with API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import './App.css';
 import { Introduction } from './components/introduction/introduction'
+import { useFetch } from "./hooks/useFetch";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Spinner from 'react-bootstrap/Spinner';
 
-function App() {
-  return (
-    <div className="App">
-		<header className="App-header">
-			<Introduction />
-		</header>
-    </div>
-  );
+export const MyContext = React.createContext({});
+
+export default function App() {
+	const responseSupplier = useFetch("http://hackathon.gorazd.sk/php-crud-api-master/api.php/records/Supplier/1", {}).response;
+	if (!responseSupplier) {
+	  return <Spinner animation="border" />
+	}
+	console.log('res', responseSupplier)
+
+	const state = {
+		supplier: {
+			...responseSupplier
+		}
+	}
+
+	return (
+		<div className="App">
+			<MyContext.Provider value={state}>
+				<Introduction />
+			</MyContext.Provider>
+		</div>
+	);
 }
-
-export default App;
--- src/components/introduction/introduction.jsx
+++ src/components/introduction/introduction.jsx
@@ -1,40 +1,19 @@
-import React, { useEffect, useState }  from 'react'
+import React  from 'react'
 import logo from "../../img/logo.jpeg";
+import { MyContext } from "../../App";
 
 export function Introduction() {
-	const [commitHistory, setCommitHistory] = useState([]);
-
-	useEffect(() => {
-		fetch(
-		  `https://api.github.com/search/commits?q=repo:facebook/react+css&page=${1}`,
-		  {
-			method: "GET",
-			headers: new Headers({
-			  Accept: "application/vnd.github.cloak-preview"
-			})
-		  }
-		)
-		  .then(res => res.json())
-		  .then(response => {
-			console.log('response', response.items)
-			setCommitHistory(response.items)
-		  })
-		  .catch(error => console.log(error));
-	  }, []);
-	
 	return (
-		<>
-			<h3>Restauracia Burina</h3>
-			<p>Nejake info o restike</p>
-			<img src={logo} alt=""/>
-
-			<p>facebook commits: </p>
-			<ul>
-				{commitHistory.map(commit => (
-					<li key={commit.sha}>{commit.url}</li>
-				))}
-			</ul>
-		</>
+		<MyContext.Consumer>
+			{state => (
+				<>
+					<h2>{state.supplier.Name}</h2>
+					<h3>{state.supplier.type}</h3>
+					<p>{state.supplier.info}</p>
+					<img src={logo} alt=""/>
+				</>
+			)}
+		</MyContext.Consumer>
 	)
 }
 
