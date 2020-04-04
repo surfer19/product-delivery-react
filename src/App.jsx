4170ff12903774fa3070bbf6fbca9695fca72d36
Marian <marianmrva123@gmail.com>
2020-04-04 21:56:44 +0200

    Gojko introduction

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Spinner from 'react-bootstrap/Spinner';
 import { OfferList } from "./components/offer-list/OfferList";
 import { Goodbye } from "./components/goodbye/Goodbye"
 import { CustomerForm } from "./components/customer-form/CustomerForm";
+import { Admin } from "./components/admin/Admin";
 import {
 	BrowserRouter as Router,
 	Switch,
@@ -19,18 +20,34 @@ export const GlobalContext = React.createContext({
 });
 
 export default function App() {
-	// const options = {
-	// 	method: 'GET',
-	// 	// mode: "no-cors",
-	// 	headers: { 
-	// 		// 'content-type': 'application/json', 
-	// 		'Access-Control-Allow-Origin': "*"
-	// 	}
-	// }
+	const data = {
+		value: 'Majko'
+	}
+	// Build formData object.
+	let formData = new FormData();
+	formData.append('Name', 'John');
+
+	const options = {
+		method: 'POST',
+		body: formData,
+		// headers: { 
+			// 'content-type': 'application/json', 
+			// 'Access-Control-Allow-Origin': "*"
+		// }
+	}
+	// http://fecko.org/productdelivery/{table}/{action}/{id-if-needed}
+	// 1. zistis si ktory supplier je prihlaseny 1
+	// 2. vytvor kategoriu pre supplaiera 
+	// 3. https://fecko.org/productdelivery/Product/create/1
+	// /options ->  
+	const categoryPost = useFetch("https://fecko.org/productdelivery/ProductCategory/create", options).response;
+	console.log('categoryPost', categoryPost)
 	
 	const responseSupplier = useFetch("https://fecko.org/productdelivery/Supplier/detail/1", {}).response;
 	const productCategories = useFetch("https://fecko.org/productdelivery/ProductCategory", {}).response;
 	const supplierProducts = useFetch("https://fecko.org/productdelivery/custom/supplier-products/1", {}).response;
+
+	console.log('productCategories', productCategories)
 	
 	const supplierProductsGroupedByCategory = groupSupplierProductsByCategory(productCategories, supplierProducts)
 	// console.log('GROUPED!', supplierProductsGroupedByCategory);
@@ -53,6 +70,7 @@ export default function App() {
 						<Route path="/supplier-offer" children={<OfferList />} />
 						<Route path="/customer-form" children={<CustomerForm />} />
 						<Route path="/goodbye" children={<Goodbye />} />
+						<Route path="/admin" children={<Admin/>} />
 						<Route path="/" children={<Introduction/>} />
 					</Switch>
 				</GlobalContext.Provider>
---
+++ src/components/admin/Admin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Admin() {
+	return (
+		<div>
+			SME V ADMINE
+		</div>
+	)
+}
