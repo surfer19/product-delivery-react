7013a1a91a44ff05428ee20208f876205695ed61
Marian <marianmrva123@gmail.com>
2020-04-05 02:24:59 +0200

    Add logic about basket

--- src/components/offer-list/OfferList.jsx
+++ src/components/offer-list/OfferList.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, {useContext, useState} from 'react'
 import { Link } from "react-router-dom";
 import { GlobalContext } from "../../App";
 import { isEmpty } from 'ramda';
+import { ContactContext } from "../../context";
+import { BottomBar } from "../bottom-bar/BottomBar"
+
+export function ProductItem({product, addItemToBasket, removeItemfromBasket, recalculateTotalPrice}) {
+	const [count, setCount] = useState(0)
+	return (
+		<li key={product.ProductID}>
+			{product.Name}
+			{product.Price}€
+			{product.Description}
+			<span>{count}</span>
+			<button onClick={() =>{ 
+				addItemToBasket(product)
+				setCount(count+1)
+				recalculateTotalPrice()
+			}}
+			>+</button>
+			<button onClick={() =>{ 
+				removeItemfromBasket(product.ProductID)
+				setCount(count > 0 ? count-1 : count)
+				recalculateTotalPrice()
+			}}
+			> -
+			</button>
+		</li>
+	)
+}
 
 export function OfferList() {	
+	const [state, dispatch] = useContext(ContactContext);
+	console.log('state', state)
+	
+	const renderCategoryProducts = (categoryProducts) => {
+		if (!categoryProducts || isEmpty(categoryProducts.listProducts)) return "Žiadna ponuka pre tento deň";
+		return categoryProducts.listProducts.map(product => (
+			<ProductItem
+				product={product}
+				addItemToBasket={addItemToBasket}
+				removeItemfromBasket={removeItemfromBasket}
+				recalculateTotalPrice={recalculateTotalPrice}
+			/>
+		))
+	}
+	
+	const removeItemfromBasket = id => {
+		dispatch({
+			type: "REMOVE_FROM_BASKET",
+		    payload: id
+		});
+	}
+
+	const addItemToBasket = item => {
+		console.log('itemitem', item)
+		dispatch({
+			type: "ADD_TO_BASKET",
+		    payload: item
+		});
+	  };
+
+	const recalculateTotalPrice = () => {
+		dispatch({
+			type: "RECALCULATE_TOTAL_PRICE",
+		});
+	}
+
 	return (
 		<GlobalContext.Consumer>
 			{state => (
@@ -21,6 +84,7 @@ export function OfferList() {
 							)
 						})}
 					</ul>
+					<BottomBar />
 					<Link to="/customer-form">
 						<button>
 							vase udaje
@@ -30,18 +94,5 @@ export function OfferList() {
 			)}
 		</GlobalContext.Consumer>
 	)
-}
-
-const renderCategoryProducts = (categoryProducts) => {
-	if (!categoryProducts || isEmpty(categoryProducts.listProducts)) return "Žiadna ponuka pre tento deň";
-	return categoryProducts.listProducts.map(product => (
-		<li key={product.ProductID}>
-			{product.Name}
-			{product.Price}€
-			{product.Description}
-			<span>-</span>
-			<button>+</button>
-			<button>-</button>
-		</li>
-	))
+	
 }
