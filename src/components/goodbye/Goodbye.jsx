e28f7da07db1c5e08591370023385c3c372bde94
Gorazd Ratulovsky <gorazd.ratulovsky@gmail.com>
2020-04-20 02:03:23 +0200

    small fix in goodbye page

--- src/components/goodbye/Goodbye.jsx
+++ src/components/goodbye/Goodbye.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 export function Goodbye() {
 	return (
-		<div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column',height: '100vh'}}>
-			<h3 style={{color: '#a9bf06'}}>Ďakujeme za objednávku :)</h3>
+		<div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column',height: '100vh', padding: '0 10px'}}>
+			<h3>Ďakujeme za objednávku :)</h3>
 			<p>V prípade akýchkoľvek zmien nás kontaktujte na tel. čísle: <strong>+421 948 033 354</strong>
 			 {/* <br></br>Vaša burina */}
 			 <br></br>Váš moment
