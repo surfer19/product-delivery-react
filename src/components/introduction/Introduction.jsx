b7c157690e6a9e6cc3433c82e0945b2a61f984be
Gorazd Ratulovsky <gorazd.ratulovsky@gmail.com>
2021-03-03 09:30:46 +0100

    edited opening time

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -15,7 +15,7 @@ export function Introduction() {
 						<h2>{state.supplier.Name}</h2>
 						<p className="subtitle">{state.supplier.type}</p>
 						{/* <p>{state.supplier.info}</p> */}
-						<p>Využite možnosť objednávky online a vychutnajte si jedlo z pohodlia a bezpečia domova. Tešíme sa na Vaše objednávky. Váš Burina tím &lt;3 <br></br><br></br>Objednávku je možné vytvoriť <strong>do 10:30 hod.</strong> aktuálneho dňa. Vyzdvihnutie a rozvoz jedla bude od pondelka do piatku <strong>od 11:00 do 15:00</strong>.</p>
+						<p>Využite možnosť objednávky online a vychutnajte si jedlo z pohodlia a bezpečia domova. Tešíme sa na Vaše objednávky. Váš Burina tím &lt;3 <br></br><br></br>Objednávku je možné vytvoriť <strong>do 10:30 hod.</strong> aktuálneho dňa. Vyzdvihnutie a rozvoz jedla bude od pondelka do piatku <strong>od 11:00 do 14:00</strong>.</p>
 						<small style={{marginTop: '20px', lineHeight: 1.3, display: 'block', color: 'grey', fontSize: '10px'}}>Rezervačný systém bol vyvorený ako podpora <br/> pre podnikateľov v dobe korona-krízy.<br/> <a style={{textDecoration: 'underline', color: 'grey'}} href="mailto:productdelivery@gorazd.sk">Kontaktný e-mail na tvorcu</a></small>
 
 						<div className="footer">
